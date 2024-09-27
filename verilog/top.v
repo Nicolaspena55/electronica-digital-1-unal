f0de1e8697af503484a5d2f2eece0f35974cb6be
Nicolaspena55 <143362267+Nicolaspena55@users.noreply.github.com>
2024-09-26 19:22:59 -0500

    Add files via upload

---
+++ verilog/top.v
@@ -0,0 +1,25 @@
+`include "./controlador_motor.v"
+`include "./pwm.v"
+module top
+(
+  input wire ts1,
+  input wire ts2,
+  input wire CLK1,
+  output wire tm1,
+  output wire tm2,
+  output wire PWM1
+  
+);
+controlador_motor micontrolador_motor (
+.s1 (ts1), 
+.s2 (ts2),
+.m1 (tm1),
+.m2 (tm2)
+);
+
+pwm mipwm (
+.CLK(CLK1),
+.PWM(PWM1),
+.PWM(PWM2)
+);
+endmodule
